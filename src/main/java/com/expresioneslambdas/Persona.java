a9656c504f66d72473e79285a3a50a3bc4ece780
osman019 <osmanortizrolon@gmail.com>
2025-03-13 14:46:33 -0500

    feat: :fire: Initial new Proyect "Expresiones lamdba"

---
+++ src/main/java/com/expresioneslambdas/Persona.java
@@ -0,0 +1,20 @@
+package com.expresioneslambdas;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    public Persona(String nombre, int edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+    }
+    public String getNombre() {
+    return nombre;
+    }
+    public int getEdad() {
+    return edad;
+    }
+    @Override
+    public String toString() {
+    return nombre + " - " + edad;
+    }
+    }
